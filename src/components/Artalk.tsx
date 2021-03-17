bbf0e18829e5fbd53579438bc5b4b83a381f5f1e
syfxlin <syfxlin@gmail.com>
2021-03-17 14:35:16 +0800

    :bug: fix: Fix page switching causes comments not to load

--- src/components/Artalk.tsx
+++ src/components/Artalk.tsx
@@ -1,5 +1,5 @@
 import { default as ArtalkComment } from "artalk";
-import React from "react";
+import React, { useEffect } from "react";
 import { useTheme } from "styled-components";
 import "artalk/dist/Artalk.css";
 
@@ -10,26 +10,22 @@ type Props = {
 
 const Artalk: React.FC<Props> = ({ pageKey, serverUrl }) => {
   const theme = useTheme() as any;
-  return (
-    <div
-      id={"comment"}
-      ref={() => {
-        // @ts-ignore
-        new ArtalkComment({
-          el: `#comment`,
-          pageKey,
-          serverUrl,
-          placeholder: "留下你的足迹 ∠( ᐛ 」∠)＿",
-          noComment: "快来成为第一个评论的人吧~",
-          readMore: {
-            pageSize: 15,
-            autoLoad: true
-          },
-          darkMode: theme.type === "dark"
-        });
-      }}
-    />
-  );
+  useEffect(() => {
+    // @ts-ignore
+    new ArtalkComment({
+      el: `#comment`,
+      pageKey,
+      serverUrl,
+      placeholder: "留下你的足迹 ∠( ᐛ 」∠)＿",
+      noComment: "快来成为第一个评论的人吧~",
+      readMore: {
+        pageSize: 15,
+        autoLoad: true
+      },
+      darkMode: theme.type === "dark"
+    });
+  }, [pageKey, serverUrl]);
+  return <div id={"comment"} />;
 };
 
 export default Artalk;
