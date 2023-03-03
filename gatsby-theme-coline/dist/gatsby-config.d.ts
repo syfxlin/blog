2e1fbeb04a7601738eb2dc6a3a431bfc47821fe2
syfxlin <i@ixk.me>
2023-03-04 01:52:17 +0800

    fix: remote ignore files

--- gatsby-theme-coline/dist/gatsby-config.d.ts
+++
@@ -1,5 +0,0 @@
-import { GatsbyConfig } from 'gatsby';
-
-declare const config: () => GatsbyConfig;
-
-export { config as default };
--- gatsby-theme-coline/dist/gatsby-node.d.ts
+++
@@ -1,6 +0,0 @@
-import { GatsbyNode } from 'gatsby';
-
-declare const onCreateNode: GatsbyNode["onCreateNode"];
-declare const createPages: GatsbyNode["createPages"];
-
-export { createPages, onCreateNode };
