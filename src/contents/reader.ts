0787ab2aa474e749e4f8b69cd632efd14e420273
syfxlin <i@ixk.me>
2023-12-23 23:40:05 +0800

    refactor: without bundle

--- src/contents/reader.ts
+++ src/contents/reader.ts
@@ -1,5 +1,4 @@
-import fs from "fs-extra";
 import config from "../../keystatic.config";
 import { createReader } from "@keystatic/core/reader";
 
-export const reader = createReader(fs.pathExistsSync("public") ? "." : "../", config);
+export const reader = createReader(".", config);
