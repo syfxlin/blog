b70925d6b7ae584edc869f9067f992aeecb9ddf3
syfxlin <i@ixk.me>
2023-12-25 00:29:11 +0800

    feature: update hero font to caveat

--- src/components/layouts/hero/index.tsx
+++ src/components/layouts/hero/index.tsx
@@ -1,14 +1,18 @@
 import * as styles from "./styles.css";
 import React from "react";
+import { Caveat } from "next/font/google";
 import { Image } from "../../ui/image";
+import { cx } from "@syfxlin/reve";
 import { fetcher } from "../../../contents";
+
+const caveat = Caveat({ subsets: ["latin"] });
 
 export const Hero: React.FC = async () => {
   const author = await fetcher.author();
   return (
     <section className={styles.section}>
       <Image className={styles.avatar} src={author.avatar} alt="头像" />
-      <h1 className={styles.author}>{author.fullname}</h1>
+      <h1 className={cx(styles.author, caveat.className)}>{author.fullname}</h1>
       <p className={styles.description}>{author.description}</p>
     </section>
   );
