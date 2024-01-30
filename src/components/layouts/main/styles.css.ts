44cc489192ce7060c6c1cc4f9e7573418d383343
syfxlin <i@ixk.me>
2024-01-31 02:07:15 +0800

    fix(style): Fix some styles

--- src/components/layouts/main/styles.css.ts
+++ src/components/layouts/main/styles.css.ts
@@ -8,6 +8,26 @@ export const container = styled.css`
   width: 100%;
   padding: 0 ${theme.spacing.calc(8)};
   animation: fade-in 0.5s ease;
+`;
+
+export const sm = styled.css`
+  max-width: ${theme.breakpoint.sm};
+
+  ${theme.media.xl} {
+    max-width: ${theme.breakpoint.sm};
+  }
+
+  ${theme.media.lg} {
+    max-width: ${theme.breakpoint.sm};
+  }
+
+  ${theme.media.md} {
+    max-width: ${theme.breakpoint.sm};
+  }
+`;
+
+export const md = styled.css`
+  max-width: ${theme.breakpoint.md};
 
   ${theme.media.xl} {
     max-width: ${theme.breakpoint.md};
@@ -16,20 +36,24 @@ export const container = styled.css`
   ${theme.media.lg} {
     max-width: ${theme.breakpoint.sm};
   }
-  
+
   ${theme.media.md} {
-    max-width: ${theme.breakpoint.xs};
+    max-width: ${theme.breakpoint.sm};
   }
-`;
-
-export const sm = styled.css`
-  max-width: ${theme.breakpoint.sm};
-`;
-
-export const md = styled.css`
-  max-width: ${theme.breakpoint.md};
 `;
 
 export const lg = styled.css`
   max-width: ${theme.breakpoint.lg};
+
+  ${theme.media.xl} {
+    max-width: ${theme.breakpoint.md};
+  }
+
+  ${theme.media.lg} {
+    max-width: ${theme.breakpoint.sm};
+  }
+
+  ${theme.media.md} {
+    max-width: ${theme.breakpoint.sm};
+  }
 `;
