e4bb5a3b09a96ae224f92e3f6cb80e0f97786800
syfxlin <i@ixk.me>
2024-01-31 01:23:53 +0800

    fix(style): Fix some styles

--- src/components/layouts/main/styles.css.ts
+++ src/components/layouts/main/styles.css.ts
@@ -4,9 +4,32 @@ import { theme } from "../../../theme/theme.css";
 export const container = styled.css`
   flex: 1;
   position: relative;
-  margin: 0 auto;
+  margin: 100px auto 0;
   width: 100%;
-  max-width: ${theme.fontSize.calc(50)};
   padding: 0 ${theme.spacing.calc(8)};
   animation: fade-in 0.5s ease;
+
+  ${theme.media.xl} {
+    max-width: ${theme.breakpoint.md};
+  }
+
+  ${theme.media.lg} {
+    max-width: ${theme.breakpoint.sm};
+  }
+  
+  ${theme.media.md} {
+    max-width: ${theme.breakpoint.xs};
+  }
+`;
+
+export const sm = styled.css`
+  max-width: ${theme.breakpoint.sm};
+`;
+
+export const md = styled.css`
+  max-width: ${theme.breakpoint.md};
+`;
+
+export const lg = styled.css`
+  max-width: ${theme.breakpoint.lg};
 `;
